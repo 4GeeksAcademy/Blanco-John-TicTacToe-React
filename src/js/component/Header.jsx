3ce5a382f5f63b840f70a40235feadd56c87985c
blancojohn <142819007+blancojohn@users.noreply.github.com>
2023-10-21 14:41:54 +0000

    Creación Botones Weapons

--- src/js/component/Header.jsx
+++ src/js/component/Header.jsx
@@ -3,13 +3,18 @@ import React from "react";
 const Header = () => {
     return (
         <>
-            <div className="container">
+            <div className="container position-absolute top-50 start-50 translate-middle">
                 <div ClassName="Formulario">
                     <h1>CHOOSE YOUR WEAPON</h1>
                     <form>
                         <input ClassName="PlayerUnoUsarname" type="text" value="" placeholder="Name Player One" />
                         <input ClassName="PlayerDosUsername" type="text" value="" placeholder="Name Player Two" />
                     </form>
+                    <div className="Weapons">
+                        <button type="button" class="btn btn-secondary">Secondary</button>
+                        <button type="button" class="btn btn-secondary">Secondary</button>
+                    </div>
+
                 </div>
             </div>
         </>
